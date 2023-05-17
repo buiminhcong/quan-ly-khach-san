470c8e3f4a424a0656ea434083bc5fb2013a3304
cong9c5s <cong9c5s@gmail.com>
2023-05-17 21:24:36 +0700

    done

---
+++ src/main/java/com/example/hotel/service/HotelService.java
@@ -0,0 +1,56 @@
+package com.example.hotel.service;
+
+import com.example.hotel.dto.*;
+import com.example.hotel.dto.bill.InfoBillDTO;
+import com.example.hotel.dto.servicedto.OrderServiceDTO;
+import com.example.hotel.dto.servicedto.OrderServiceResponse;
+import com.example.hotel.dto.stat.StatDTO;
+import com.example.hotel.dto.stat.StatDTO2;
+import com.example.hotel.dto.stat.StatDTO3;
+import com.example.hotel.model.RoomModel;
+import com.example.hotel.model.ServiceModel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface HotelService {
+    CreateRoomDTO create(CreateRoomDTO dto);
+
+    List<RoomModel> getAllRoom();
+
+    RoomModel update(UUID id, UpdateRoomDTO dto);
+
+    RoomModel blockRoom(UUID id);
+
+    RoomModel unlockRoom(UUID id);
+
+    void approve(UUID bookingID, Float saleoff);
+
+    void cancel(UUID bookingID);
+
+    void checkin(UUID bookingID);
+
+    List<BookingListDTO> getBooking();
+
+    ServiceModel createService(ServiceCreateDTO dto);
+
+    ServiceModel updateService(UUID serviceID, ServiceCreateDTO dto);
+
+    ServiceModel inactiveService(UUID serviceID);
+
+    ServiceModel activeService(UUID serviceID);
+
+    List<ServiceModel> getAllService();
+
+    //service
+    OrderServiceResponse orderService(UUID bookingID, List<OrderServiceDTO> dtos);
+
+    InfoBillDTO payment(UUID bookingID);
+
+    //stat
+    List<StatDTO> statService();
+
+    List<StatDTO3> statRoom();
+
+    List<StatDTO2> statBill();
+}
